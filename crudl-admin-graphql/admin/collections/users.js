ef7e298d32c0bb721972cb214a3c82c7f315462d
Patrick Kranzlmueller <sehmaschine@gmail.com>
2016-08-11 17:04:34 +0200

    different addview fieldset with user

--- crudl-admin-graphql/admin/collections/users.js
+++ crudl-admin-graphql/admin/collections/users.js
@@ -75,7 +75,6 @@ var changeView = {
 }
 
 changeView.fieldsets = [
-
     {
         fields: [
             {
@@ -186,12 +185,86 @@ changeView.fieldsets = [
 var addView = {
     path: 'users/new',
     title: 'New User',
-    fieldsets: changeView.fieldsets,
     denormalize: changeView.denormalize,
     actions: {
         add: function (req) { return crudl.connectors.users.create(req) },
     },
 }
+
+addView.fieldsets = [
+    {
+        fields: [
+            {
+                name: 'username',
+                label: 'Username',
+                field: 'String',
+            },
+        ],
+    },
+    {
+        fields: [
+            {
+                name: 'first_name',
+                label: 'Name',
+                field: 'String',
+            },
+            {
+                name: 'last_name',
+                label: 'Last Name',
+                field: 'String',
+            },
+            {
+                name: 'email',
+                label: 'Email address',
+                field: 'String',
+            }
+        ],
+    },
+    {
+        title: 'Roles',
+        expanded: true,
+        fields: [
+            {
+                name: 'is_active',
+                label: 'Active',
+                field: 'Checkbox',
+                initialValue: true,
+                props: {
+                    helpText: 'Designates whether this user should be treated as active. Unselect this instead of deleting accounts.'
+                },
+            },
+            {
+                name: 'is_staff',
+                label: 'Staff member',
+                field: 'Checkbox',
+                props: {
+                    helpText: 'Designates whether the user can log into crudl.'
+                },
+            },
+        ],
+    },
+    {
+        title: 'Password',
+        expanded: true,
+        fields: [
+            {
+                name: 'password',
+                label: 'Password',
+                field: 'Password',
+            },
+            {
+                name: 'password_confirm',
+                label: 'Password (Confirm)',
+                field: 'Password',
+                validate: (value, allValues) => {
+                    if (value != allValues.password) {
+                        return 'The passwords do not match.'
+                    }
+                }
+            },
+        ]
+    }
+]
 
 
 module.exports = {
--- crudl-admin-rest/admin/collections/users.js
+++ crudl-admin-rest/admin/collections/users.js
@@ -174,11 +174,85 @@ changeView.fieldsets = [
 var addView = {
     path: 'users/new',
     title: 'New User',
-    fieldsets: changeView.fieldsets,
     actions: {
         add: function (req) { return crudl.connectors.users.create(req) },
     },
 }
+
+addView.fieldsets = [
+    {
+        fields: [
+            {
+                name: 'username',
+                label: 'Username',
+                field: 'String',
+            },
+        ],
+    },
+    {
+        fields: [
+            {
+                name: 'first_name',
+                label: 'Name',
+                field: 'String',
+            },
+            {
+                name: 'last_name',
+                label: 'Last Name',
+                field: 'String',
+            },
+            {
+                name: 'email',
+                label: 'Email address',
+                field: 'String',
+            }
+        ],
+    },
+    {
+        title: 'Roles',
+        expanded: true,
+        fields: [
+            {
+                name: 'is_active',
+                label: 'Active',
+                field: 'Checkbox',
+                initialValue: true,
+                props: {
+                    helpText: 'Designates whether this user should be treated as active. Unselect this instead of deleting accounts.'
+                },
+            },
+            {
+                name: 'is_staff',
+                label: 'Staff member',
+                field: 'Checkbox',
+                props: {
+                    helpText: 'Designates whether the user can log into crudl.'
+                },
+            },
+        ],
+    },
+    {
+        title: 'Password',
+        expanded: true,
+        fields: [
+            {
+                name: 'password',
+                label: 'Password',
+                field: 'Password',
+            },
+            {
+                name: 'password_confirm',
+                label: 'Password (Confirm)',
+                field: 'Password',
+                validate: (value, allValues) => {
+                    if (value != allValues.password) {
+                        return 'The passwords do not match.'
+                    }
+                }
+            },
+        ]
+    }
+]
 
 
 module.exports = {
