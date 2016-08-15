738ebe0c03d702dfdb300ac87cb68e24e8bfe577
Patrick Kranzlmueller <sehmaschine@gmail.com>
2016-08-15 15:28:52 +0200

    dashboard updates

--- crudl-admin-rest/admin/dashboard.jsx
+++ crudl-admin-rest/admin/dashboard.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
 
 export default (
-    <div>
-        <h2>About</h2>
-        <p>
-            This is a <a href="http://crudl.io/">crudl</a> example with <a href="http://expressjs.com/">Express</a> and a REST-API as well as GraphQL.
-        </p>
+    <div className="mgs-container">
+        <div className="mgs-row">
+            <div className="box">
+                <h3>About</h3>
+                <p>
+                    This is a <a href="http://crudl.io/" target="_blank">crudl</a> example with <a href="http://expressjs.com/" target="_blank">Express</a> and a REST-API as well as GraphQL.
+                </p>
+                <p>
+                    crudl is a backend agnostic React application in order to rapidly build a beautiful administration interface based on your API (REST or GraphQL).
+                </p>
+                <p>
+                    Please note that crudl is still under development and not production ready.
+                </p>
+                <p>
+                    A temporary documentation — outlining the basics and most important features — is available on <a href="https://github.com/crudlio/crudl-example-express/blob/master/README.md" target="_blank">GitHub</a>.
+                </p>
+                <h3>Credits & Links</h3>
+                <ul>
+                    <li><a href="http://crudl.io" target="_blank">crudl.io</a></li>
+                    <li><a href="https://twitter.com/crudlio" target="_blank">Twitter</a></li>
+                    <li><a href="https://github.com/crudlio" target="_blank">GitHub</a></li>
+                </ul>
+            </div>
+        </div>
     </div>
 )
